b67d665a557df960ce62b072fc0837e49e1def34
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-11-17 15:08:59 -0500

    Update/Delete Functions Added

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.soit.soitfaculty.entity.Faculty;
 import com.soit.soitfaculty.service.FacultyService;
 
@@ -47,6 +49,19 @@ public class FacultyController {
 		return "faculties/faculty-form";
 	}
 	
+	@GetMapping("/viewUpdateForm")
+	public String viewUpdateForm(@RequestParam("facultyId") int theId, Model theModel) {
+		
+		//retrieve faculty info from service layer
+		Faculty theFaculty = facultyService.findById(theId);
+	
+		//Pre-populate the form by setting faculty as a model attribute
+		theModel.addAttribute("faculty", theFaculty);
+		
+		//Redirect to faculty form
+		return "faculties/faculty-form";
+	}
+	
 	@PostMapping("/save")
 	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
 		
@@ -54,6 +69,14 @@ public class FacultyController {
 		facultyService.save(theFaculty);
 		
 		//Block duplicate submission for accidental refresh
+		return "redirect:/Faculties/list";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("facultyId") int theId) {
+		//Remove Faculty
+		facultyService.deleteById(theId);
+		//Redirect to Faculty list
 		return "redirect:/Faculties/list";
 	}
 }
